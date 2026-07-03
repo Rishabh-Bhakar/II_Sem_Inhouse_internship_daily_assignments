7726730fb55f4d453a7e7622b43d9de88a28fa34
Rishabh-Bhakar <bhakarrishabh111@gmail.com>
2026-07-03 16:06:42 +0530

    All Day Folder Added For Roll No 25ESKCS284

---
+++ 25ESKCS284/Day-3/script.js
@@ -0,0 +1,10 @@
+const firstName = "Rishabh";
+const lastName = "Bhakar";
+const studentId = "b250793";
+const year = 2;
+const gpa = 8.5;
+console.log(
+  `-----Student Profile----\nName=${firstName} ${lastName}\nStudent id=${studentId}\nYear=${year}\nGPA=${gpa}`,
+);
+
+
